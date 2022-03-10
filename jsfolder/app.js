8540f7276994f3ba9a6695f09f095014d9c8496d
sodiqkhamdamov <91616796+sodiqkhamdamov@users.noreply.github.com>
2022-03-10 23:15:47 +0500

    push

--- jsfolder/app.js
+++ jsfolder/app.js
@@ -1,10 +1,16 @@
 const atmena = document.querySelector(".atmena");
 const zamena = document.querySelector(".zamena");
+const links = document.querySelector(".malumot");
+const empty = document.querySelector(".empty");
 atmena.addEventListener("click", () => {
   zamena.classList.remove("active");
   atmena.classList.add("active");
+  empty.classList.remove("hidden");
+  links.classList.add("hidden")
 });
 zamena.addEventListener("click", () => {
   atmena.classList.remove("active");
   zamena.classList.add("active");
+  empty.classList.add("hidden");
+  links.classList.remove("hidden")
 });
